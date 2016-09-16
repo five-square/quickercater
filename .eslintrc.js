f71f3e3fdeafff7f958c3c3dc2db76f16531c780
francistoto <fbt.github@gmail.com>
2016-09-16 09:55:22 -0500

    Added a style guide, modified version of AirBnB

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+      "browser": true,
+      "node": true,
+      "es6": true
+    },
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx"] }],
+    }
+};
