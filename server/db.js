33822a3a325e4aa92bc68774e2082738a10e06ed
Abel Costilla <aecostilla@gmail.com>
2016-09-17 12:43:59 -0500

    Fixed lint syle errors on db helper functions

--- server/db.js
+++ server/db.js
@@ -1,37 +1,34 @@
 const Promise = require('bluebird');
+
 const neo4j = Promise.promisifyAll(require('neo4j'));
+
 const Node = new neo4j.GraphDatabase({ url: process.env.GRAPHENEDB_URL || 'http://neo4j:start@localhost:7474' });
+
 const db = module.exports;
 
-db.createOwner = (owner => {
-  return Node.cypherAsync({
-    query: `MERGE (profile:Profile 
+db.createOwner = (owner => Node.cypherAsync({
+  query: `MERGE (profile:Profile 
     { name: {name}, phone: {phone}, email: {email}, desc: {desc}, auth_key: {auth_key} })
-     RETURN profile`,
-     params: {
-      name: owner.name,
-      phone: owner.phone,
-      email: owner.email,
-      desc: owner.desc,
-      auth_key: owner.auth_key,
-    },
-  });
-});
+    RETURN profile`,
+  params: {
+    name: owner.name,
+    phone: owner.phone,
+    email: owner.email,
+    desc: owner.desc,
+    auth_key: owner.auth_key,
+  },
+}));
 
-db.findOwner = (ownerName => {
-  return Node.cypherAsync({
-    query: `MATCH (profile:Profile { name: {name} }) RETURN profile`,
-    params: {
-      name: ownerName,
-    },
-  });
-});
+db.findOwner = (ownerName => Node.cypherAsync({
+  query: `MATCH (profile:Profile { name: {name} }) RETURN profile`,
+  params: {
+    name: ownerName,
+  },
+}));
 
-db.deleteOwner = (ownerName => {
-  return Node.cypherAsync({
-    query: `MATCH (profile:Profile { name: {name} }) DELETE profile`,
-    params: {
-      name: ownerName,
-    },
-  });
-});
+db.deleteOwner = (ownerName => Node.cypherAsync({
+  query: `MATCH (profile:Profile { name: {name} }) DELETE profile`,
+  params: {
+    name: ownerName,
+  },
+}));
