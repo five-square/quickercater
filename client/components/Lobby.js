37e416f4d99694c8b2e97886f5ccb37b9aade133
Abel Costilla <aecostilla@gmail.com>
2016-09-20 15:37:59 -0500

    Styled store cards

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -14,16 +14,24 @@ export default class Lobby extends Component {
   componentWillMount() {
     Server.getAllStores()
     .then(stores => {
-      console.log('Stores in CWM: ', stores);
       this.setState({ stores });
     });
   }
   render() {
+    const style = {
+      width: 60 + '%',
+      flex: 50 + '%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
     return (
       <div className="Lobby" >
         <h1>Welcome!</h1>
         { this.state.stores.map((e) =>
-          <StoreCard key={e._id} stores={e.properties} />
+          <StoreCard key={e._id} style={style} stores={e.properties} />
         )}
       </div>
   );
