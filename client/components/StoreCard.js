f901a5ca41808235e3e5c158ccbb4fa7908c19c9
Abel Costilla <aecostilla@gmail.com>
2016-09-20 09:31:44 -0500

    Added animation for StoreCard on hover

--- client/components/StoreCard.js
+++ client/components/StoreCard.js
@@ -10,6 +10,8 @@ import CardText from 'material-ui/Card/CardText';
 
 import FlatButton from 'material-ui/FlatButton';
 
+import Paper from 'material-ui/Paper';
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
@@ -19,39 +21,40 @@ export default class StoreCardExpandable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expandable: true,
-      expanded: false,
+      hover: 3,
       name: this.props.owners.name,
       slogan: this.props.owners.slogan,
       description: this.props.owners.description,
     };
   }
 
-  handleExpanded() {
-    if (this.sate.expanded) {
-      this.setState({ expanded: false });
-    } else {
-      this.setState({ expanded: true });
-    }
+  handleOnMouseEnter() {
+    this.setState({ hover: 5 });
+  }
+
+  handleOnMouseLeave() {
+    this.setState({ hover: 3 });
   }
 
   render() {
     return (
-      <Card>
-        <CardHeader
-          title={this.state.name}
-          subtitle={this.state.slogan}
-          avatar="https://ssl.gstatic.com/images/branding/product/1x/avatar_circle_blue_512dp.png"
-          actAsExpander={true}
-          showExpandableButton={true}
-        />
-        <CardText expandable={this.state.expandable} onTouchTap={this.handleExpanded} >
+      <Paper zDepth={this.state.hover}>
+        <Card onMouseEnter={e => this.handleOnMouseEnter(e)} onMouseLeave={e => this.handleOnMouseLeave(e)}>
+          <CardHeader
+            title={this.state.name}
+            subtitle={this.state.slogan}
+            avatar="https://ssl.gstatic.com/images/branding/product/1x/avatar_circle_blue_512dp.png"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+        <CardText expandable={true}>
           {this.state.description}
         </CardText>
-        <CardActions>
-          <FlatButton label="Take Me There" />
-        </CardActions>
-      </Card>
+          <CardActions>
+            <FlatButton label="Take Me There" />
+          </CardActions>
+        </Card>
+      </Paper>
     );
   }
 }
