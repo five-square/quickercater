f104101b15a825f54e0018175ec317d5134c3ca0
tgvinodkumar <tgvinodkumar@gmail.com>
2016-09-29 12:41:45 -0500

    moved the create order method from createOrderAPI to orderAPI and deleted createOrderAPI

--- client/models/orderAPI.js
+++ client/models/orderAPI.js
@@ -76,5 +76,16 @@ orderAPI.updateOrder = (order, items, removedItems) =>
   })
   .then(resp => resp.json());
 
+orderAPI.create = (orderInfo) => {
+  console.log('CreateOrderAPI orderInfo: ', orderInfo);
+  return fetch('/api/order/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(orderInfo),
+  })
+  .then(data => data.json());
+};
 
 export default orderAPI;
