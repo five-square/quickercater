e8133ffe9f7a9f580a56bf214261103f0665a898
Frank Toto <fbt.github@gmail.com>
2016-10-01 15:46:59 -0500

    fixed typos

--- client/components/PackageCard.js
+++ client/components/PackageCard.js
@@ -30,7 +30,6 @@ const PackageCard = (props) => (
       <img role="presentation" src={props.pack.picture} />
     </CardMedia>
     {props.editing
-<<<<<<< ac61ff4d5092bfe598f9e2e57d154d2d1f081883
       ? <div style={style.floatingEditButton}>
         <EditPackage
           edit={props.handleSubmitEdit}
@@ -47,7 +46,6 @@ const PackageCard = (props) => (
       </div>
       : null
     }
-=======
               ? <div style={style.floatingEditButton}>
                 <EditPackage
                   edit={props.handleSubmitEdit}
@@ -64,7 +62,6 @@ const PackageCard = (props) => (
               </div>
               : null
             }
->>>>>>> added files
   </Card>
 );
 
