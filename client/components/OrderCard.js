315746bf11937b64cccd98e377d6ecd715ab279f
tgvinodkumar <tgvinodkumar@gmail.com>
2016-10-04 14:27:09 -0500

    refactored OrderCard to use if else block in lieu of ternary

--- client/components/OrderCard.js
+++ client/components/OrderCard.js
@@ -124,6 +124,118 @@ export default class OrderCard extends React.Component {
     this.setState({ reviewOrder: false, open: false });
   }
 
+  renderComponent() {
+    const style = {
+      cancelBtn: {
+        right: 30,
+        bottom: 0,
+        position: 'absolute',
+      },
+      card: {
+        margin: 1,
+        width: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        position: 'relative',
+      },
+    };
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary
+        onTouchTap={e => this.handleCancel(e)}
+      />,
+      <FlatButton
+        label="Submit"
+        primary
+        keyboardFocused
+        onTouchTap={e => this.handleSubmit(e)}
+      />,
+    ];
+    if (this.state.reviewOrder === false) {
+      return (<div>
+        <RaisedButton
+          primary label="Submit"
+          onTouchTap={e => this.handleOpen(e)}
+        />
+        <FlatButton
+          primary label="Cancel"
+          style={style.cancelBtn}
+          onTouchTap={e => this.handleRemoveOrder(e)}
+        />
+        <Dialog
+          title="Please enter your information"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={(e) => this.handleClose(e)}
+        >
+          <TextField
+            ref="customerName"
+            hintText="Your name"
+            floatingLabelText="Your name"
+            floatingLabelFixed
+          />
+          <br />
+          <TextField
+            ref="customerEmail"
+            hintText="Email"
+            floatingLabelText="Email"
+            floatingLabelFixed
+          />
+          <br />
+          <TextField
+            ref="customerPhone"
+            hintText="Phone"
+            floatingLabelText="Phone"
+            floatingLabelFixed
+            errorText={this.state.errorTextPhone}
+            onChange={e => this.onChangePhone(e)}
+          />
+          <br />
+          <TextField
+            ref="ordername"
+            hintText="Order name"
+            floatingLabelText="Order name"
+            floatingLabelFixed
+          />
+          <br />
+          Request Date
+          <DatePicker
+            ref="requestDate"
+            hintText="Date Picker"
+            onChange={(e, date) => this.handleRequestDate(e, date)}
+          />
+          <br />
+          <TextField
+            ref="orderAddress"
+            hintText="Order address"
+            floatingLabelText="Address"
+            type="text"
+          />
+          <h4>{`Price: $${this.props.orderInfo.totalPrice}`}</h4>
+        </Dialog>
+      </div>);
+    } else if (this.state.submitted) {
+      return (<OrderConfirmation
+        orderId={this.state.newOrder}
+        storeName={this.props.storeName}
+        showMe
+        ownerId={this.props.ownerId}
+        resetAfterConfirmation={e => this.resetAfterConfirmation(e)}
+      />);
+    }
+    return (<OrderDetails
+      showMe
+      orderInfo={this.state.orderInfo}
+      orderState={'customerView'}
+      handleOrderAccept={e => this.handleOrderAccept(e)}
+      handleOrderReject={e => this.handleOrderReject(e)}
+      handleModalCancel={e => this.handleModalCancel(e)}
+    />);
+  }
+
+
   render() {
     const style = {
       cancelBtn: {
@@ -139,105 +251,9 @@ export default class OrderCard extends React.Component {
         position: 'relative',
       },
     };
-    // action buttons for Modal
-    console.log('this.props.orderInfo: ', this.props.orderInfo);
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary
-        onTouchTap={e => this.handleCancel(e)}
-      />,
-      <FlatButton
-        label="Submit"
-        primary
-        keyboardFocused
-        onTouchTap={e => this.handleSubmit(e)}
-      />,
-    ];
-    // This is the actual modal
     return (
       <div style={style.card}>
-        { this.state.reviewOrder === false
-          ? <div>
-            <RaisedButton
-              primary label="Submit"
-              onTouchTap={e => this.handleOpen(e)}
-            />
-            <FlatButton
-              primary label="Cancel"
-              style={style.cancelBtn}
-              onTouchTap={e => this.handleRemoveOrder(e)}
-            />
-            <Dialog
-              title="Please enter your information"
-              actions={actions}
-              modal={false}
-              open={this.state.open}
-              onRequestClose={(e) => this.handleClose(e)}
-            >
-              <TextField
-                ref="customerName"
-                hintText="Your name"
-                floatingLabelText="Your name"
-                floatingLabelFixed
-              />
-              <br />
-              <TextField
-                ref="customerEmail"
-                hintText="Email"
-                floatingLabelText="Email"
-                floatingLabelFixed
-              />
-              <br />
-              <TextField
-                ref="customerPhone"
-                hintText="Phone"
-                floatingLabelText="Phone"
-                floatingLabelFixed
-                errorText={this.state.errorTextPhone}
-                onChange={e => this.onChangePhone(e)}
-              />
-              <br />
-              <TextField
-                ref="ordername"
-                hintText="Order name"
-                floatingLabelText="Order name"
-                floatingLabelFixed
-              />
-              <br />
-              Request Date
-              <DatePicker
-                ref="requestDate"
-                hintText="Date Picker"
-                onChange={(e, date) => this.handleRequestDate(e, date)}
-              />
-              <br />
-              <TextField
-                ref="orderAddress"
-                hintText="Order address"
-                floatingLabelText="Address"
-                type="text"
-              />
-              <h4>{`Price: $${this.props.orderInfo.totalPrice}`}</h4>
-            </Dialog>
-          </div>
-        : this.state.submitted
-            ? <OrderConfirmation
-              orderId={this.state.newOrder}
-              storeName={this.props.storeName}
-              showMe
-              ownerId={this.props.ownerId}
-              resetAfterConfirmation={e => this.resetAfterConfirmation(e)}
-            />
-            : <OrderDetails
-              showMe
-              orderInfo={this.state.orderInfo}
-              orderState={'customerView'}
-              handleOrderAccept={e => this.handleOrderAccept(e)}
-              handleOrderReject={e => this.handleOrderReject(e)}
-              handleModalCancel={e => this.handleModalCancel(e)}
-            />
-      }
+        {this.renderComponent()}
       </div>);
   }
 }
