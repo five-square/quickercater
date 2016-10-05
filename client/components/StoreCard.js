3574034bb8e7f88a52d68dec5fe340ea7920b8f7
Abel Costilla <aecostilla@gmail.com>
2016-10-05 01:07:05 -0500

    Code clean up in store card and made the card clickable and deleted store description

--- client/components/StoreCard.js
+++ client/components/StoreCard.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Card from 'material-ui/Card';
-import CardActions from 'material-ui/Card/CardActions';
 import CardHeader from 'material-ui/Card/CardHeader';
-import CardText from 'material-ui/Card/CardText';
-import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 
 export default class StoreCard extends React.Component {
@@ -47,20 +44,14 @@ export default class StoreCard extends React.Component {
             // style={{ backgroundColor: 'rgba(255, 255, 255, .89)' }}
             onMouseEnter={e => this.handleOnMouseEnter(e)}
             onMouseLeave={e => this.handleOnMouseLeave(e)}
+            onTouchTap={e => this.handleClick(e)}
           >
             <CardHeader
               title={this.props.store.name}
               subtitle={this.props.store.slogan}
               avatar={pic}
-              actAsExpander
-              showExpandableButton
+
             />
-            <CardText expandable>
-              {this.props.store.description}
-            </CardText>
-            <CardActions>
-              <FlatButton label="Take Me There" onTouchTap={e => this.handleClick(e)} />
-            </CardActions>
           </Card>
         </Paper>
         <br />
