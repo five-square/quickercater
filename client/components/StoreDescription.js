5a28d14661816bf4f3e3adf4c3631b508a3523c9
Abel Costilla <aecostilla@gmail.com>
2016-10-06 11:56:01 -0500

    Added Letter to store descriptions with no logo

--- client/components/StoreDescription.js
+++ client/components/StoreDescription.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
 import EditStore from './EditStore';
-
+// change two buttons together
 export default class StoreDescription extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  handleStoreLogo() {
+    const style = {
+      position: 'relative',
+      left: 18,
+      bottom: 150,
+      height: 100,
+      width: 100,
+      letter: {
+        height: 100,
+        width: 100,
+        fontSize: 50,
+        position: 'relative',
+        left: 20,
+        bottom: 83,
+      },
+    };
+    let logo = this.props.store.picture;
+    if (logo === '' || logo === false) {
+      logo = (<Avatar style={style.letter} children={this.props.store.name.charAt(0)} />);
+    } else {
+      logo = (<img alt="logo" style={style} src={this.props.store.picture} />);
+    }
+    return logo;
+  }
+
   render() {
     const style = {
       width: '80%',
@@ -26,17 +53,25 @@ export default class StoreDescription extends React.Component {
       img: {
         position: 'relative',
         left: 18,
-        top: 14,
+        bottom: 150,
         height: 100,
         width: 100,
+      },
+      name: {
+        marginTop: 20,
+      },
+      desc: {
+        marginLeft: '19%',
+        marginRight: '20%',
+        marginBottom: 0,
       },
     };
     return (
       <div>
         <Paper style={style} zDepth={1} rounded={false}>
-          <img alt="logo" style={style.img} src={this.props.store.picture} />
-          <h1 style={{ marginTop: '-8%' }}>{this.props.store.name}</h1>
-          <h3 style={{ marginLeft: '19%', marginRight: '20%', marginBottom: 0 }}>{this.props.store.description}</h3>
+          <h1 style={style.name}>{this.props.store.name}</h1>
+          <h3 style={style.desc}>{this.props.store.description}</h3>
+          {this.handleStoreLogo()}
           {this.props.editing ?
             <EditStore
               edit={this.props.editing}
