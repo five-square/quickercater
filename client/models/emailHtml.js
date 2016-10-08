c605bc0bcff5759ec8a16ca92237f3678f72a8cd
tgvinodkumar <tgvinodkumar@gmail.com>
2016-10-08 09:21:24 -0500

    added event date and time to the email body to be sent to the customer

--- client/models/emailHtml.js
+++ client/models/emailHtml.js
@@ -16,6 +16,8 @@ Email.compose = (orderInfo, storeName, context) => {
 
   emailHtml.push('<div><p>Below are your order details:</p></div>');
   emailHtml.push(`<div><h3>Order number: ${orderInfo.order.id}</h3></div>`);
+  emailHtml.push(`<div><h4>Event time & date: ${orderInfo.order.start_time} 
+                  ${orderInfo.order.request_date}</h4></div>`);
   emailHtml.push(`<div>
                   <table>
                   <thead>
